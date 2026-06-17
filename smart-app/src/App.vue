aa04ec0e9f5d05c2d58f872a4dc71439f94e1ba3
jinwei <614774796@qq.com>
2026-06-17 14:47:07 +0800

    feat: 小程序增加版本更新

--- smart-app/src/App.vue
+++ smart-app/src/App.vue
@@ -3,6 +3,10 @@
   import { initMessageLocalNotification, setAppVisible } from '@/lib/message-local-notification';
   export default {
     async onLaunch() {
+      // 小程序版本更新检测（仅小程序环境生效）
+      // #ifdef MP-WEIXIN || MP-ALIPAY || MP-BAIDU || MP-TOUTIAO
+      this.checkUpdate();
+      // #endif
       initMessageLocalNotification();
       await useUserStore().getLoginInfo();
     },
@@ -16,6 +20,66 @@
     },
     onHide: function () {
       setAppVisible(false);
+    },
+    methods: {
+      /**
+       * 检测小程序版本更新，有新版自动下载并引导用户重启
+       */
+      checkUpdate() {
+        // #ifdef MP-WEIXIN
+        if (wx.getUpdateManager) {
+          const updateManager = wx.getUpdateManager();
+          updateManager.onCheckForUpdate((res) => {
+            if (res.hasUpdate) {
+              console.log('[更新] 检测到新版本');
+            }
+          });
+          updateManager.onUpdateReady(() => {
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已准备好，是否立即重启小程序？',
+              showCancel: false,
+              confirmText: '重启',
+              success: (modalRes) => {
+                if (modalRes.confirm) {
+                  updateManager.applyUpdate();
+                }
+              },
+            });
+          });
+          updateManager.onUpdateFailed(() => {
+            console.log('[更新] 新版本下载失败');
+          });
+        }
+        // #endif
+
+        // #ifdef MP-ALIPAY
+        if (my.getUpdateManager) {
+          const updateManager = my.getUpdateManager();
+          updateManager.onCheckForUpdate((res) => {
+            if (res.hasUpdate) {
+              console.log('[更新] 检测到新版本');
+            }
+          });
+          updateManager.onUpdateReady(() => {
+            my.confirm({
+              title: '更新提示',
+              content: '新版本已准备好，是否立即重启？',
+              confirmButtonText: '重启',
+              cancelButtonText: '稍后',
+              success: (modalRes) => {
+                if (modalRes.confirm) {
+                  updateManager.applyUpdate();
+                }
+              },
+            });
+          });
+          updateManager.onUpdateFailed(() => {
+            console.log('[更新] 新版本下载失败');
+          });
+        }
+        // #endif
+      },
     },
   };
 </script>
